6812b23b61d6b878662b249c3390d4feb74c773f
RohitNegi59 <168254077+RohitNegi59@users.noreply.github.com>
2024-08-24 16:51:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,610 @@
+    
+function show(event) {
+    let button1 = document.getElementById("btn1");
+    let button2 = document.getElementById("btn2");
+    let button3 = document.getElementById("btn3");
+    let button4 = document.getElementById("btn4");
+    let button5 = document.getElementById("btn5");
+    let button6 = document.getElementById("btn6");
+    let button7 = document.getElementById("btn7");
+    let button8 = document.getElementById("btn8");
+    let button9 = document.getElementById("btn9");
+    let button10 = document.getElementById("btn10");
+    let button11 = document.getElementById("btn11");
+    let button12 = document.getElementById("btn12");
+    let button13 = document.getElementById("btn13");
+    let button14 = document.getElementById("btn14");
+    let button15 = document.getElementById("btn15");
+    let button16 = document.getElementById("btn16");
+    let button17 = document.getElementById("btn17");
+    let button18 = document.getElementById("btn18");
+    let button19 = document.getElementById("btn19");
+    let button20 = document.getElementById("btn20");
+    let button21 = document.getElementById("btn21");
+    let button22 = document.getElementById("btn22");
+    let button23 = document.getElementById("btn23");
+    let button24 = document.getElementById("btn24");
+    let button25 = document.getElementById("btn25");
+    let button26 = document.getElementById("btn26");
+    
+
+
+     
+
+    rightanswer=function(){
+        alert("you are right");
+    };
+    wronganswer=function(){
+        alert("oops wrong answer: Please try again");
+    }
+
+
+
+    let x = event.currentTarget;
+
+
+
+    if (x === button1) {
+
+        console.log("this is button 1 ");
+        const inputElement = document.getElementById("myInput0");
+        const inputValue = inputElement.value;
+        console.log("Input value:", inputValue);
+        const seb="apple";
+        if(inputValue===seb){
+            rightanswer(inputValue,event);
+        }else{
+            wronganswer(inputValue,event);
+        }
+      
+
+    }
+    else if (x === button2) {
+        console.log("this is button 2 ");
+        const inputElement = document.getElementById("myInput1");
+        const inputValue = inputElement.value;
+        console.log("Input value:", inputValue);
+        const ball="ball";
+        if(inputValue===ball){
+            rightanswer(inputValue,event);
+        }else{
+            wronganswer(inputValue,event);
+        }
+      
+    }
+    else if (x === button3) {
+        console.log("this is button 3 ");
+        const inputElement = document.getElementById("myInput2");
+        const inputValue = inputElement.value;
+        console.log("Input value:", inputValue);
+        const cat="cat";
+        const birol="birol"
+        if(inputValue===cat|| inputValue===birol){
+            rightanswer(inputValue,event);
+        }else{
+            wronganswer(inputValue,event);
+        }
+      
+    }
+
+    else if (x === button4) {
+        const alpha="Apple";
+        console.log("this is button 4 ");
+        const inputElement = document.getElementById("myInput3");
+        const inputValue = inputElement.value;
+        
+        console.log("Input value:", inputValue);
+        const dog="dog";
+        if(inputValue===dog){
+            rightanswer(inputValue,event);
+        }else{
+            wronganswer(inputValue,event);
+        }
+      
+
+    }
+    else if (x === button5) {
+        console.log("this is button 5 ");
+        const inputElement = document.getElementById("myInput4");
+        const inputValue = inputElement.value;
+        console.log("Input value:", inputValue);
+        const elephant="elephant";
+        if(inputValue===elephant){
+            rightanswer(inputValue,event);
+        }else{
+            wronganswer(inputValue,event);
+        }
+      
+    }
+    else if (x === button6) {
+        console.log("this is button 5 ");
+        const inputElement = document.getElementById("myInput5");
+        const inputValue = inputElement.value;
+        console.log("Input value:", inputValue);
+        const fish="fish";
+        if(inputValue===fish){
+            rightanswer(inputValue,event);
+        }else{
+            wronganswer(inputValue,event);
+        }
+      
+    }
+    else if (x === button7) {
+        console.log("this is button 7 ");
+        const inputElement = document.getElementById("myInput6");
+        const inputValue = inputElement.value;
+        console.log("Input value:", inputValue);
+        const giraffe="giraffe";
+        if(inputValue===giraffe){
+            rightanswer(inputValue,event);
+        }else{
+            wronganswer(inputValue,event);
+        }
+      
+    }
+    else if (x === button8) {
+        console.log("this is button 8 ");
+        const inputElement = document.getElementById("myInput7");
+        const inputValue = inputElement.value;
+        console.log("Input value:", inputValue);
+        const hen="hen";
+        const murga="murga";
+        if(inputValue===hen||inputValue===murga){
+            rightanswer(inputValue,event);
+        }else{
+            wronganswer(inputValue,event);
+        }
+      
+    }
+    else if (x === button9) {
+        console.log("this is button 9 ");
+        const inputElement = document.getElementById("myInput8");
+        const inputValue = inputElement.value;
+        console.log("Input value:", inputValue);
+        const icecream="icecream";
+        if(inputValue===icecream){
+            rightanswer(inputValue,event);
+        }else{
+            wronganswer(inputValue,event);
+        }
+      
+    }
+    else if (x === button10) {
+        console.log("this is button 10 ");
+        const inputElement = document.getElementById("myInput9");
+        const inputValue = inputElement.value;
+        console.log("Input value:", inputValue);
+        const jug="jug";
+        if(inputValue===jug){
+            rightanswer(inputValue,event);
+        }else{
+            wronganswer(inputValue,event);
+        }
+      
+    }
+    else if (x === button11) {
+        console.log("this is button 11 ");
+        const inputElement = document.getElementById("myInput10");
+        const inputValue = inputElement.value;
+        console.log("Input value:", inputValue);
+        const kite="kite";
+        if(inputValue===kite){
+            rightanswer(inputValue,event);
+        }else{
+            wronganswer(inputValue,event);
+        }
+      
+    }
+    else if (x === button12) {
+        console.log("this is button 12 ");
+        const inputElement = document.getElementById("myInput11");
+        const inputValue = inputElement.value;
+        console.log("Input value:", inputValue);
+        const lion="lion";
+        if(inputValue===lion){
+            rightanswer(inputValue,event);
+        }else{
+            wronganswer(inputValue,event);
+        }
+      
+    }
+    else if (x === button13) {
+        console.log("this is button 13 ");
+        const inputElement = document.getElementById("myInput12");
+        const inputValue = inputElement.value;
+        console.log("Input value:", inputValue);
+        const monkey="monkey";
+        if(inputValue===monkey){
+            rightanswer(inputValue,event);
+        }else{
+            wronganswer(inputValue,event);
+        }
+      
+    }
+    else if (x === button14) {
+        console.log("this is button 14 ");
+        const inputElement = document.getElementById("myInput13");
+        const inputValue = inputElement.value;
+        console.log("Input value:", inputValue);
+        const nest="nest";
+        if(inputValue===nest){
+            rightanswer(inputValue,event);
+        }else{
+            wronganswer(inputValue,event);
+        }
+      
+    }
+    else if (x === button15) {
+        console.log("this is button 15 ");
+        const inputElement = document.getElementById("myInput14");
+        const inputValue = inputElement.value;
+        console.log("Input value:", inputValue);
+        const orange="orange";
+        if(inputValue===orange){
+            rightanswer(inputValue,event);
+        }else{
+            wronganswer(inputValue,event);
+        }
+      
+    }
+    else if (x === button16) {
+        console.log("this is button 16 ");
+        const inputElement = document.getElementById("myInput15");
+        const inputValue = inputElement.value;
+        console.log("Input value:", inputValue);
+        const parrot="parrot";
+        if(inputValue===parrot){
+            rightanswer(inputValue,event);
+        }else{
+            wronganswer(inputValue,event);
+        }
+      
+    }
+    else if (x === button17) {
+        console.log("this is button 17 ");
+        const inputElement = document.getElementById("myInput16");
+        const inputValue = inputElement.value;
+        console.log("Input value:", inputValue);
+        const queen="queen";
+        if(inputValue===queen){
+            rightanswer(inputValue,event);
+        }else{
+            wronganswer(inputValue,event);
+        }
+      
+    }
+    else if (x === button18) {
+        console.log("this is button 18 ");
+        const inputElement = document.getElementById("myInput17");
+        const inputValue = inputElement.value;
+        console.log("Input value:", inputValue);
+        const rabbit="rabbit";
+        if(inputValue===rabbit){
+            rightanswer(inputValue,event);
+        }else{
+            wronganswer(inputValue,event);
+        }
+      
+    }
+    else if (x === button19) {
+        console.log("this is button 18 ");
+        const inputElement = document.getElementById("myInput18");
+        const inputValue = inputElement.value;
+        console.log("Input value:", inputValue);
+        const ship="ship";
+        if(inputValue===ship){
+            rightanswer(inputValue,event);
+        }else{
+            wronganswer(inputValue,event);
+        }
+      
+    }
+    else if (x === button20) {
+        console.log("this is button 18 ");
+        const inputElement = document.getElementById("myInput19");
+        const inputValue = inputElement.value;
+        console.log("Input value:", inputValue);
+        const tiger="tiger";
+        if(inputValue===tiger){
+            rightanswer(inputValue,event);
+        }else{
+            wronganswer(inputValue,event);
+        }
+      
+    }
+    else if (x === button21) {
+        console.log("this is button 18 ");
+        const inputElement = document.getElementById("myInput20");
+        const inputValue = inputElement.value;
+        console.log("Input value:", inputValue);
+        const umbrella="umbrella";
+        if(inputValue===umbrella){
+            rightanswer(inputValue,event);
+        }else{
+            wronganswer(inputValue,event);
+        }
+      
+    }
+    else if (x === button22) {
+        console.log("this is button 22 ");
+        const inputElement = document.getElementById("myInput21");
+        const inputValue = inputElement.value;
+        console.log("Input value:", inputValue);
+        const van="van";
+        if(inputValue===van){
+            rightanswer(inputValue,event);
+        }else{
+            wronganswer(inputValue,event);
+        }
+      
+    }
+    else if (x === button23) {
+        console.log("this is button 23 ");
+        const inputElement = document.getElementById("myInput22");
+        const inputValue = inputElement.value;
+        console.log("Input value:", inputValue);
+        const watch="watch";
+        if(inputValue===watch){
+            rightanswer(inputValue,event);
+        }else{
+            wronganswer(inputValue,event);
+        }
+      
+    }
+    else if (x === button24) {
+        console.log("this is button 24 ");
+        const inputElement = document.getElementById("myInput23");
+        const inputValue = inputElement.value;
+        console.log("Input value:", inputValue);
+        const xmastree="xmastree";
+        if(inputValue===xmastree){
+            rightanswer(inputValue,event);
+        }else{
+            wronganswer(inputValue,event);
+        }
+      
+    }
+    else if (x === button25) {
+        console.log("this is button 25 ");
+        const inputElement = document.getElementById("myInput24");
+        const inputValue = inputElement.value;
+        console.log("Input value:", inputValue);
+        const yak="yak";
+        if(inputValue===yak){
+            rightanswer(inputValue,event);
+        }else{
+            wronganswer(inputValue,event);
+        }
+      
+    }
+    else if (x === button26) {
+        console.log("this is button 25 ");
+        const inputElement = document.getElementById("myInput25");
+        const inputValue = inputElement.value;
+        console.log("Input value:", inputValue);
+        const zebra="zebra";
+        if(inputValue===zebra){
+            rightanswer(inputValue,event);
+        }else{
+            wronganswer(inputValue,event);
+        }
+      
+    }
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const btn1=document.querySelector("#apple");
+
+
+
+
+// btn1.onclick=(event)=>{
+// console.log(event)
+// const Inputa = document.getElementById("a");
+//   const userName = Inputa.value;
+//   if (userName === "Apple" || userName === "apple") {
+//     alert("Correct: You are a genius!");
+//   }
+//   else{
+//     alert("oops your anwer is worng Please try again");
+//   }
+   
+// };
+
+// button=document.querySelector("button");
+// // input=document.querySelector(input);
+// // userInput=input.value;
+// button.onclick=(event)=>{
+  
+//   // input=document.querySelector(input);
+//   // userInput=input.value;
+//    console.log(event);
+     
+//   };
+
+
+
+
+
+
+
+
+
+// // const userInput1 = document.getElementById("apple").value;
+// // const userInput2 = document.getElementById("ball").value;
+// // const userInput3 = document.getElementById("cat").value;
+
+
+// // function checkapple(){
+// //     console.log("userInput1");
+// //     // const userInput1 = document.getElementById("apple1");
+// //     // console.log(userInput1);
+  
+
+    
+
+// // }
+
+
+
+// // function check2(){
+
+// //     const userInput2 = document.getElementById("ball").value;
+  
+// //     if (userInput2 === "Ball" || userInput1 === "ball") {
+// //         alert("Correct: You are a genius!");}
+    
+// //     else{
+// //         alert("wrong anwer");
+// //     }
+// // }
+
+
+// // function check3(){
+
+// //     const userInput3 = document.getElementById("cat").value;
+  
+// //     if ((userInput3 == "Cat" || userInput1 === "cat") {
+// //         alert("Correct: You are a genius!");}
+    
+// //     else{
+// //         alert("wrong anwer");
+// //     }
+// // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // function check() {
+// //     const userInput = document.getElementById("myInput").value;
+
+// //     if (userInput === "Apple" || userInput === "apple") {
+// //         alert("Correct : you are a genius ");
+// //     }
+// //     else{
+// //         alert("Worng Answer: oops ")
+// //     }
+
+// // }
+
+
+
+// // function check(buttonNumber) {
+// //     const userInput = document.getElementById("myInput").value;
+
+// //     if (buttonNumber === 1) {
+// //         // Code specific to button 1
+// //         if (userInput === "Apple" || userInput === "apple") {
+// //             alert("Correct: You are a genius!");
+// //         } else {
+// //             alert("Wrong Answer: Oops!");
+// //         }
+// //     } else if (buttonNumber === 2) {
+// //         if (userInput === "Ball" || userInput === "ball") {
+// //             alert("Correct: You are a genius!");
+// //         } else {
+// //             alert("Wrong Answer: Oops!");
+// //         }
+        
+// //     }
+// //     else if (buttonNumber === 3) {
+// //         if (userInput === "Cat" || userInput === "cat") {
+// //             alert("Correct: You are a genius!");
+// //         } else {
+// //             alert("Wrong Answer: Oops!");
+// //         }
+        
+// //     }
+// // }
+
+
+
+
+// // function check(buttonNumber) {
+// //     const userInput = document.getElementById("myInput").value;
+
+// //     switch (buttonNumber) {
+// //         case 1:
+// //             if (userInput === "Apple" || userInput === "apple") {
+// //                 alert("Correct: You are a genius!");
+// //             } else {
+// //                 alert("Wrong Answer: Oops!");
+// //             }
+// //             break;
+// //         case 2:
+// //             if (userInput === "Ball" || userInput === "ball") {
+// //                 alert("Correct: You are a genius!");
+// //             } else {
+// //                 alert("Wrong Answer: Oops!");
+// //             }
+// //             break;
+// //         case 3:
+// //             if (userInput === "Cat" || userInput === "cat") {
+// //                 alert("Correct: You are a genius!");
+// //             } else {
+// //                 alert("Wrong Answer: Oops!");
+// //             }
+// //             break;
+// //         default:
+// //             alert("Invalid button number");
+// //     }
+// // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
